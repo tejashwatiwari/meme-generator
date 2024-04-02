52d1e01e9432e6a5d0de9daf5becc6ff62e6659a
tejashwatiwari <tejashwatiwari@gmail.com>
2024-04-01 23:47:34 -0700

    fixed footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Gag Genius © 2024 <a href="https://github.com/tejashwatiwari" target="_blank" rel="noopener noreferrer">
-          Tejashwa Tiwari
-        </a></p>
+      <p>Gag Genius © 2024 || <a href="https://github.com/tejashwatiwari" target="_blank" rel="noopener noreferrer">Tejashwa Tiwari</a></p>
       <p>
         
       </p>
